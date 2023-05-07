d190f657da00cfdd4c1f9d813307e8be13a07c6f
Pratham Choudhary <choudharyy.pratham@gmail.com>
2023-05-07 19:53:29 +0530

    Squats (Code Chef)

---
+++ Code chef/squats.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,sum=0;
+	cin>>t;
+	while(t!=0)
+	{
+	    cin>>x;
+	    sum = 15*x;
+	    cout<<sum<<endl;;
+	    t--;
+	}
+	return 0;
+}
