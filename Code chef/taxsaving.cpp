b8a8b23551511fa7670a04f92659e457215f5e8d
Pratham Choudhary <choudharyy.pratham@gmail.com>
2023-05-07 19:59:19 +0530

    Saving TAX (Code Chef)

---
+++ Code chef/taxsaving.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t!=0)
+	{
+	    cin>>x>>y;
+	    if(x>y)
+	    {
+	        cout<<x-y<<endl;
+	    }
+	    t--;
+	}
+	return 0;
+}
