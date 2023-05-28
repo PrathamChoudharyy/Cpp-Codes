06cd29eb640da8a2870e497ead1fcd2ee925d4a1
Pratham Choudhary <choudharyy.pratham@gmail.com>
2023-05-28 10:52:41 +0530

    Sum.cpp
    Sum of two numbers entered by the User

---
+++ Programiz Codes/Sum.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter the Numbers: ";
+    cin>>a>>b;
+    sum = a+b;
+    cout<<"The Sum of "<<a<<" and "<<b<<" is: "<<sum;
+    return 0;
+}
