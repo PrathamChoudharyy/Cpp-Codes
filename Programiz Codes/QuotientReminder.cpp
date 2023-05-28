003724fa499c88548491e2dc0637994d62628ee4
Pratham Choudhary <choudharyy.pratham@gmail.com>
2023-05-28 11:00:26 +0530

    Create QuotientReminder.cpp
    Finding the Quotient and Reminder of the user entered Divisor and Divident

---
+++ Programiz Codes/QuotientReminder.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,rem,q;
+    cout<<"Enter the Divisor and Divedent: ";
+    cin>>a>>b;
+    q=a/b;
+    rem=a%b;
+    cout<<"Quotient: "<<q<<" and Reminder: "<<rem<<endl;
+    return 0;
+}
